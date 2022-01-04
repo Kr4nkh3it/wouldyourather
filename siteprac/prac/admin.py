5caedbbdd1b0f5a9870de50e508fdc446109d2cb
Krankheit <dawsonjohnathanc@gmail.com>
2022-01-03 19:52:57 -0500

    adminchange
    changes to admin view

--- siteprac/prac/admin.py
+++ siteprac/prac/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Question,Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 0
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question',{'fields':['question_text']}),
+        ('Date',{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question,QuestionAdmin)
--- siteprac/siteprac/settings.py
+++ siteprac/siteprac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9!t3ppyme1s_($4*3+(+%g=@pu=_+m(%&8ju2-1=6-5k0diowz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.23','127.0.0.1']
 
 
 # Application definition
